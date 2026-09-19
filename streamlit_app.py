622b5372c15578e60c26daa8f90d95adae49cb65
ubedkhn <ubedkhanuit@gmail.com>
2026-09-19 20:13:46 +0530

    Updated text to Version 2.0

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,4 @@ import streamlit as st
 st.title("My CI/CD Test App 🚀")
 st.write("Hello! This app is deployed using GitHub Actions and Render.")
 st.write("Current Version: 1.0 (Main Branch)")
+st.write("Current Version: 2.0 (Updated via CI/CD!)")
